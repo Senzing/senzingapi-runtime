3233d19e086801c7615c1eba15f5aad598f57add
chia hui mah <mahchiahui@hotmail.sg>
2022-11-10 23:48:05 +0800

    #35 add test script

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check ENV for LD_LIBRARY_PATH
+if [[ -z "${LD_LIBRARY_PATH}" ]]; then
+  echo "Environment variable LD_LIBRARY_PATH is not set"
+  exit 0
+else
+  MY_SCRIPT_VARIABLE="${DEPLOY_ENV}"
+
+# Verify that some Senzing files have been installed
+# /opt/senzing/g2/g2BuildVersion.json  (log contents)
+FILE=/opt/senzing/g2/g2BuildVersion.json
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist."
+fi
+
+# /opt/senzing/data/libpostal/data_version
+FILE=/opt/senzing/data/libpostal/data_version
+if test -f "$FILE"; then
+    echo "$FILE exists."
+else
+    echo "$FILE does not exist."
+fi
