63cd5fbc7cf4c6167203f988b6004c6d5c5beb81
chia hui mah <mahchiahui@hotmail.sg>
2022-11-14 23:08:37 +0800

    #35 test new codebuild creds

--- test.sh
+++ test.sh
@@ -22,6 +22,15 @@ FILE=/opt/senzing/data/libpostal/data_version
 if test -f "$FILE"; then
     echo "$FILE exists."
 else
-    echo "$FILE does not exist."
+    echo "$FILE does not exist. "
     exit 0
 fi
+
+# parse /opt/senzing/g2/g2BuildVersion.json, get BUILD_VERSION and compare it with SENZING_APT_INSTALL_PACKAGE="senzingapi-runtime=3.3.1-22283"
+# {
+#     "PLATFORM": "Linux",
+#     "VERSION": "3.3.2",
+#     "BUILD_VERSION": "3.3.2.22299",
+#     "BUILD_NUMBER": "2022_10_26__19_38",
+#     "DATA_VERSION": "3.0.0"
+# }
